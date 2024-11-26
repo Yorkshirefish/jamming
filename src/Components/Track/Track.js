2b6cb1ca85bffa696e3a578ef98b280906cedb51
Will Fisher <willem.fisher98@gmail.com>
2024-11-26 20:22:59 +0000

    Building Track and Tracklist Components

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Track() {
     return(
-        <div>
-
+        <div className="Track">
+            <div className="Track-information">
+                <h3>{/* track name will go here */}</h3>
+                <p>{/*track artist will go here*/}{/* track album will go here */}</p>
+            </div>
+            <button className="Track-action">{isRemoval ? '-' : '+'}</button>
         </div>
     )
 }
