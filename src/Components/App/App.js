7ef7b383d11c2f8913a5bcb05c96d8aa2dd16732
Will Fisher <willem.fisher98@gmail.com>
2024-11-27 12:53:55 +0000

    Creating addTrack function in app.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,26 +6,34 @@ import Playlist from '../Playlist/Playlist';
 
 function App() {
 
-const [searchResults, setSearchResults] = useState([
-  { name: 'Song 1', artist: 'Artist 1', album: 'Album 1', id: 1 },
-  { name: 'Song 2', artist: 'Artist 2', album: 'Album 2', id: 2 },
-  { name: 'Song 3', artist: 'Artist 3', album: 'Album 3', id: 3 }
-]);
+  const [searchResults, setSearchResults] = useState([
+    { name: 'Song 1', artist: 'Artist 1', album: 'Album 1', id: 1 },
+    { name: 'Song 2', artist: 'Artist 2', album: 'Album 2', id: 2 },
+    { name: 'Song 3', artist: 'Artist 3', album: 'Album 3', id: 3 }
+  ]);
 
-const [playlistName, setPlaylistName] = useState('My Playlist');
+  const [playlistName, setPlaylistName] = useState('My Playlist');
 
-const [playlistTracks, setPlaylistTracks] = useState([
-  { name: 'Track 1', artist: 'Artist A', album: 'Album A', id: 4 },
-  { name: 'Track 2', artist: 'Artist B', album: 'Album B', id: 5 }
-]);
+  const [playlistTracks, setPlaylistTracks] = useState([
+    { name: 'Track 1', artist: 'Artist A', album: 'Album A', id: 4 },
+    { name: 'Track 2', artist: 'Artist B', album: 'Album B', id: 5 }
+  ]);
+
+  
+  function addTrack(track) {
+    if(!playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+      setPlaylistTracks(prevTrack => [...prevTrack, track]);
+    }
+  }
+
 
   return (
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
       <SearchBar/>
-        <div class="App-playlist">
-      <SearchResults searchResults={searchResults}/>
+        <div className="App-playlist">
+      <SearchResults searchResults={searchResults} onAdd={addTrack}/>
       <Playlist playlistName={playlistName} playlistTracks={playlistTracks}/>
         </div>
       </div>
